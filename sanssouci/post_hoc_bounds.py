f6dde8a0e0150ea8b4d05f3a02d3048bbb17bfe1
alexblnn <you@example.com>
2022-05-09 10:39:53 +0200

    fix bound equal case

--- sanssouci/post_hoc_bounds.py
+++ sanssouci/post_hoc_bounds.py
@@ -61,7 +61,7 @@ def max_fp(p_values, thresholds):
 
     card = np.zeros(thresholds.shape[0])
     for i in range(thresholds.shape[0]):
-        card[i] = np.sum(p_values > thresholds[i])
+        card[i] = np.sum(p_values >= thresholds[i])
 
     return np.min([subset_size, (card + seq_k).min()])
 
